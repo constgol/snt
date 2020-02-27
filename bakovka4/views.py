96f235974be149d8234801999674e92d19fbf8dc
Konstantin Vyshegorodtsev <const-gol@mail.ru>
2020-02-27 20:58:17 +0300

    purpose by year / month templates and views

--- bakovka4/views.py
+++ bakovka4/views.py
@@ -4,11 +4,11 @@ from django.db.models import Sum
 from django.db.models.functions import Extract
 from .models import PaymentChr
 from .models import PAYMENT_PURPOSE
+from datetime import date
 
 
 def purposeIndex(request):
     p_list = dict(PAYMENT_PURPOSE)
-    p_list[''] = '-'
     purpose_list = PaymentChr.objects. \
         exclude(site=None). \
         values('purpose', purpose_year=Extract('pay_date', 'year')). \
@@ -58,6 +58,36 @@ def purposeIndex(request):
 
 def purposeByYear(request, purpose):
     p_list = dict(PAYMENT_PURPOSE)
+    purpose_list = PaymentChr.objects. \
+        exclude(site=None). \
+        filter(purpose=purpose). \
+        values('site', purpose_year=Extract('pay_date', 'year')). \
+        annotate(total=Sum('amount'))
+    pp = {}
+    total = 0
+    yp = {}
+    for row in purpose_list:
+        if row['site'] in pp:
+            pp[row['site']]['total'] += row['total']
+            pp[row['site']]['purpose_year'][row['purpose_year']] = row['total']
+        else:
+            pp[row['site']] = {
+                'name': row['site'],
+                'total': row['total'],
+                'purpose_year': {
+                    row['purpose_year']: row['total']
+                }
+            }
+
+        if row['purpose_year'] in yp:
+            yp[row['purpose_year']] += row['total']
+        else:
+            yp[row['purpose_year']] = row['total']
+        total += row['total']
+
+    years = list(yp.keys())
+    years.sort()
+
     ba = admin.site
     ba._build_app_dict(request)
     app_label = 'bakovka4'
@@ -67,14 +97,52 @@ def purposeByYear(request, purpose):
         'app_list': [app_dict],
         'app_label': app_label,
         'purpose': purpose,
+        'purpose_list': pp.items(),
         'purpose_name': p_list[purpose],
-        'title': 'Назначение платежа',
+        'total': total,
+        'title': p_list[purpose],
+        'years': years,
+        'years_list': yp.items(),
+        **({}),
     }
     return render(request, 'bakovka4/purpose_by_year.html', context)
 
 
 def purposeByMonth(request, purpose, year):
     p_list = dict(PAYMENT_PURPOSE)
+    purpose_list = PaymentChr.objects. \
+        exclude(site=None). \
+        filter(purpose=purpose). \
+        filter(pay_date__year=year). \
+        values('site', purpose_month=Extract('pay_date', 'month')). \
+        annotate(total=Sum('amount'))
+    pp = {}
+    total = 0
+    mp = {}
+    for row in purpose_list:
+        if row['site'] in pp:
+            pp[row['site']]['total'] += row['total']
+            pp[row['site']]['purpose_month'][row['purpose_month']] = row['total']
+        else:
+            pp[row['site']] = {
+                'name': row['site'],
+                'total': row['total'],
+                'purpose_month': {
+                    row['purpose_month']: row['total']
+                }
+            }
+
+        if row['purpose_month'] in mp:
+            mp[row['purpose_month']] += row['total']
+        else:
+            mp[row['purpose_month']] = row['total']
+        total += row['total']
+
+    months = list(range(1, 13))
+    m_list = []
+    for m in months:
+        m_list.append(date(2020, m, 1))
+
     ba = admin.site
     ba._build_app_dict(request)
     app_label = 'bakovka4'
@@ -84,8 +152,13 @@ def purposeByMonth(request, purpose, year):
         'app_list': [app_dict],
         'app_label': app_label,
         'purpose': purpose,
+        'purpose_list': pp.items(),
         'purpose_name': p_list[purpose],
         'year': year,
-        'title': 'Назначение платежа',
+        'total': total,
+        'title': p_list[purpose] + ' (' + str(year) + ')',
+        'months': months,
+        'months_list': mp,
+        'm_list': m_list,
     }
     return render(request, 'bakovka4/purpose_by_month.html', context)
