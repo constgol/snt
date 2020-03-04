676c2a5238d15bf0ad47910cd6450aece243c1b6
Konstantin Vyshegorodtsev <const-gol@mail.ru>
2020-03-04 20:23:37 +0300

    for land membership charges show charges reference at right

--- bakovka4adm/admin.py
+++ bakovka4adm/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 
 class Bakovka4AdminSite(admin.AdminSite):
     site_header = 'Баковка 4'
+    site_title = 'Баковка 4'
 
 
 Bakovka4AdminSite(name='bakovka4adm')
-
